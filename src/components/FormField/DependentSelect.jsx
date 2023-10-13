cfd2c727750503dcad4924edb4a03690b69fbc7c
Richard Alcaide <richardalcaide16@gmail.com>
2023-10-13 17:58:00 +0800

    No option available

--- src/components/FormField/DependentSelect.jsx
+++ src/components/FormField/DependentSelect.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import './style.css'
+import './style.css';
 
 const DependentSelect = ({ id, value, data, defaultOption, onChange, className }) => {
   return (
     <div className='select-box'>
       <select id={id} value={value} onChange={onChange} className={`custom-select ${className}`}>
-      <option value="" hidden>{defaultOption}</option> 
-        {data.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
+        <option value="" hidden>{defaultOption}</option>
+        {data.length === 0 ? (
+          <option disabled>--- No options available ---</option>
+        ) : (
+          data.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))
+        )}
       </select>
       <div className='select-arrow-down'></div>
     </div>
