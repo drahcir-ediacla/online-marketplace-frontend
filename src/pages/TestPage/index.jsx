e01a0089035c5ca85a6bba612ed3b21b5f8f6865
drahcir-ediacla <richardalcaide16@gmail.com>
2023-11-02 19:52:04 +0800

    EditProfile

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -58,80 +58,141 @@
 
 
 
+// import React, { useState } from 'react';
+
+// const options = [
+//   { value: 'option1', label: 'Option 1', fields: ['field1', 'field2'] },
+//   { value: 'option2', label: 'Option 2', fields: ['field3'] },
+//   { value: 'option3', label: 'Option 3', fields: ['field4', 'field5'] },
+//   { value: 'option4', label: 'Option 4', children: [
+//     { value: 'suboption1', label: 'Suboption 1', fields: ['field6', 'field7'] },
+//     { value: 'suboption2', label: 'Suboption 2', fields: ['field8'] },
+//   ]}
+// ];
+
+// function CustomSelect() {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [selectedOption, setSelectedOption] = useState('');
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const filteredOptions = options.filter(option =>
+//     option.label.toLowerCase().includes(searchTerm.toLowerCase())
+//   );
+
+//   const toggleDropdown = () => {
+//     setIsOpen(!isOpen);
+//   }
+
+//   const handleOptionClick = (option) => {
+//     setSelectedOption(option);
+//     setIsOpen(false);
+//   }
+
+//   const handleSearchChange = (e) => {
+//     setSearchTerm(e.target.value);
+//   }
+
+//   return (
+//     <div className="custom-select">
+//       <div className={`select-container ${isOpen ? 'open' : ''}`}>
+//         <div className="selected-option" onClick={toggleDropdown}>
+//           <input placeholder={selectedOption.label || 'Select an option'} readOnly/>
+//         </div>
+//         {isOpen && (
+//           <div className="options">
+//             <input
+//               type="text"
+//               placeholder="Search..."
+//               value={searchTerm}
+//               onChange={handleSearchChange}
+//             />
+//             <ul>
+//               {filteredOptions.map((option) => (
+//                 <li
+//                   key={option.value}
+//                   onClick={() => handleOptionClick(option)}
+//                   className={option.value === selectedOption.value ? 'selected' : ''}
+//                 >
+//                   {option.label}
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//         )}
+//       </div>
+
+//       {selectedOption.fields && (
+//         <div className="form-fields">
+//           {selectedOption.fields.map((fieldName) => (
+//             <div key={fieldName}>
+//               <label>{fieldName}:</label>
+//               <input type="text" name={fieldName} />
+//             </div>
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default CustomSelect;
+
+
 import React, { useState } from 'react';
+import data from '../../data/productCategoryData.json'
 
-const options = [
-  { value: 'option1', label: 'Option 1', fields: ['field1', 'field2'] },
-  { value: 'option2', label: 'Option 2', fields: ['field3'] },
-  { value: 'option3', label: 'Option 3', fields: ['field4', 'field5'] },
-  { value: 'option4', label: 'Option 4', children: [
-    { value: 'suboption1', label: 'Suboption 1', fields: ['field6', 'field7'] },
-    { value: 'suboption2', label: 'Suboption 2', fields: ['field8'] },
-  ]}
-];
 
-function CustomSelect() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
-  const filteredOptions = options.filter(option =>
-    option.label.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  }
+function CategorySelector() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [breadcrumbPath, setBreadcrumbPath] = useState('');
 
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    setIsOpen(false);
-  }
+  const updateBreadcrumb = (categoryValue) => {
+    const selectedCategoryData = getCategoryData(categoryValue);
+    if (selectedCategoryData) {
+      const path = buildBreadcrumbPath(selectedCategoryData);
+      setBreadcrumbPath(path);
+    } else {
+      setBreadcrumbPath('');
+    }
+  };
 
-  const handleSearchChange = (e) => {
-    setSearchTerm(e.target.value);
-  }
+  const getCategoryData = (categoryValue) => {
+    return data.find((category) => category.value === categoryValue);
+  };
+
+  const buildBreadcrumbPath = (categoryData) => {
+    const path = [categoryData.label];
+    let parentCategory = getCategoryData(categoryData.value);
+    while (parentCategory) {
+      path.unshift(parentCategory.label);
+      parentCategory = getCategoryData(parentCategory.value);
+    }
+    return path.join(' > ');
+  };
+
+  const handleCategoryChange = (e) => {
+    const categoryValue = e.target.value;
+    setSelectedCategory(categoryValue);
+    updateBreadcrumb(categoryValue);
+  };
 
   return (
-    <div className="custom-select">
-      <div className={`select-container ${isOpen ? 'open' : ''}`}>
-        <div className="selected-option" onClick={toggleDropdown}>
-          <input placeholder={selectedOption.label || 'Select an option'} readOnly/>
-        </div>
-        {isOpen && (
-          <div className="options">
-            <input
-              type="text"
-              placeholder="Search..."
-              value={searchTerm}
-              onChange={handleSearchChange}
-            />
-            <ul>
-              {filteredOptions.map((option) => (
-                <li
-                  key={option.value}
-                  onClick={() => handleOptionClick(option)}
-                  className={option.value === selectedOption.value ? 'selected' : ''}
-                >
-                  {option.label}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-
-      {selectedOption.fields && (
-        <div className="form-fields">
-          {selectedOption.fields.map((fieldName) => (
-            <div key={fieldName}>
-              <label>{fieldName}:</label>
-              <input type="text" name={fieldName} />
-            </div>
-          ))}
-        </div>
-      )}
+    <div>
+      <div id="breadcrumb">{breadcrumbPath}</div>
+      <select
+        id="categorySelector"
+        value={selectedCategory}
+        onChange={handleCategoryChange}
+      >
+        <option value="">Select a Category</option>
+        {data.map((category) => (
+          <option key={category.value} value={category.value}>
+            {category.label}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
 
-export default CustomSelect;
+export default CategorySelector;
