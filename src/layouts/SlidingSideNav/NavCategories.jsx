8dfc9eaf2ef04a30c00009f6251d8c4a6f2dafa1
Richard Alcaide <richardalcaide16@gmail.com>
2023-11-28 14:34:21 +0800

    asa

--- src/layouts/SlidingSideNav/NavCategories.jsx
+++ src/layouts/SlidingSideNav/NavCategories.jsx
@@ -7,20 +7,52 @@ const NavCategories = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const response = await GetAllCategories();
-        setCategories(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+    // Check if cached data is available in localStorage
+    const cachedCategories = JSON.parse(localStorage.getItem('cachedCategories'));
 
-    fetchCategories();
+    if (cachedCategories) {
+      setCategories(cachedCategories);
+    } else {
+      const fetchCategories = async () => {
+        try {
+          const response = await GetAllCategories();
+          const fetchedCategories = response.data;
+
+          // Cache the fetched data in localStorage
+          localStorage.setItem('cachedCategories', JSON.stringify(fetchedCategories));
+
+          setCategories(fetchedCategories);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+      fetchCategories();
+    }
   }, []);
 
+  const handleDataUpdateOptimistic = async (updatedData) => {
+    // Update the client-side cache optimistically
+    const updatedCategories = categories.map((category) =>
+      category.id === updatedData.id ? updatedData : category
+    );
+
+    setCategories(updatedCategories);
+
+    try {
+      // Perform the actual data update on the server
+      await updateDataOnServer(updatedData);
+      // Server update successful
+    } catch (error) {
+      console.error('Error updating data on the server:', error);
+      // Handle errors, potentially revert the optimistic update
+      // You may want to handle error scenarios, e.g., show a notification, revert the update, etc.
+      // Depending on your application needs
+    }
+  };
 
 
+  // DROP DOWN CATEGORIES LOGIC
   const [activeCollapsible, setActiveCollapsible] = useState([]);
 
   const handleToggleCollapsible = (index) => {
--- src/layouts/SlidingSideNav/NavCategories_backup.jsx
+++ src/layouts/SlidingSideNav/NavCategories_backup.jsx
@@ -1,101 +1,77 @@
-import React, { useEffect } from 'react';
-import CategoryIcon1 from '../../assets/images/category-icon1.png'
-import CategoryIcon2 from '../../assets/images/category-icon2.png'
-import CategoryIcon3 from '../../assets/images/category-icon3.png'
-import CategoryIcon4 from '../../assets/images/category-icon4.png'
-import CategoryIcon5 from '../../assets/images/category-icon5.png'
-import CategoryIcon6 from '../../assets/images/category-icon6.png'
-import CategoryIcon7 from '../../assets/images/category-icon7.png'
-import CategoryIcon8 from '../../assets/images/category-icon8.png'
-import CategoryIcon9 from '../../assets/images/category-icon9.png'
-import CategoryIcon10 from '../../assets/images/category-icon10.png'
+import React, { useEffect, useState } from 'react';
+import { GetAllCategories } from '../../apicalls/products'
+
 
 const NavCategories = () => {
 
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
-    const collapsibleElements = document.getElementsByClassName("collapsible");
-
-    const handleCollapsibleClick = function () {
-      this.classList.toggle("active");
-      const content = this.nextElementSibling;
-
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
+    const fetchCategories = async () => {
+      try {
+        const response = await GetAllCategories();
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
     };
 
-    for (let i = 0; i < collapsibleElements.length; i++) {
-      collapsibleElements[i].addEventListener("click", handleCollapsibleClick);
-    }
-
-    return () => {
-      // Clean up event listeners when the component unmounts
-      for (let i = 0; i < collapsibleElements.length; i++) {
-        collapsibleElements[i].removeEventListener("click", handleCollapsibleClick);
-      }
-    };
+    fetchCategories();
   }, []);
 
-  
+
+
+  const [activeCollapsible, setActiveCollapsible] = useState([]);
+
+  const handleToggleCollapsible = (index) => {
+    setActiveCollapsible((prevActiveCollapsible) => {
+      const updatedCollapsible = [...prevActiveCollapsible];
+      updatedCollapsible[index] = !updatedCollapsible[index];
+
+      // Close other active collapsibles
+      updatedCollapsible.forEach((value, i) => {
+        if (i !== index) {
+          updatedCollapsible[i] = false;
+        }
+      });
+
+      return updatedCollapsible;
+    });
+  };
+
 
   return (
     <>
       <div className='nav-categories'>
         <ul>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon1} alt="" />Mobile and Electronics</div>
-          </li>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon2} alt="" />Furniture</div>
-          </li>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon3} alt="" />Home, Garden & DIY</div>
-          </li>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon4} alt="" />Baby & Kids</div>
-          </li>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon5} alt="" />Women’s Fashion</div>
-            <div className="collapsible"></div>
-            <ul className='sub-category'>
-              <li>Bottoms</li>
-              <li>Tops & Sets</li>
-              <li>Footwear</li>
-              <li>Coats & Jackets</li>
-              <li>Bags</li>
-              <li>Watches and Accessories</li>
-            </ul>
-          </li>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon6} alt="" />Men’s Fashion</div>
-            <div className="collapsible"></div>
-            <ul className='sub-category'>
-              <li>Bottoms</li>
-              <li>Tops & Sets</li>
-              <li>Footwear</li>
-              <li>Coats & Jackets</li>
-              <li>Bags</li>
-              <li>Watches and Accessories</li>
-            </ul>
-          </li>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon7} alt="" />Health & Beauty</div>
-          </li>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon8} alt="" />Sports & Leisure</div>
-          </li>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon9} alt="" />Games, Hobbies & Crafts</div>
-          </li>
-          <li className='main-category'>
-            <div className='category-icon'><img src={CategoryIcon10} alt="" />Book, Music & Tickets</div>
-          </li>
+          {categories.map((category, index) => (
+            <li className='main-category' key={index}>
+              <div className='category-icon'>
+                <img src={category.icon} alt='' />
+                {category.label}
+              </div>
+
+              {category.subcategories && category.subcategories.length > 0 && (
+                <>
+                  <div
+                    className={`collapsible ${activeCollapsible[index] ? 'active' : ''}`}
+                    onClick={() => handleToggleCollapsible(index)}
+                  ></div>
+                  {activeCollapsible[index] && (
+                    <ul className='sub-category'>
+                      {category.subcategories.map((subCategory, subIndex) => (
+                        <li key={subIndex}>{subCategory.label}</li>
+                      ))}
+                    </ul>
+                  )}
+                </>
+              )}
+            </li>
+          ))}
         </ul>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default NavCategories
