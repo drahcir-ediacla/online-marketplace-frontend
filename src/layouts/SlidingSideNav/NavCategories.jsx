8c7a67e74e63ea90842098223824d1045f370831
Richard Alcaide <richardalcaide16@gmail.com>
2023-11-28 14:35:55 +0800

    rhr

--- src/layouts/SlidingSideNav/NavCategories.jsx
+++ src/layouts/SlidingSideNav/NavCategories.jsx
@@ -1,79 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { GetAllCategories } from '../../apicalls/products'
-import { useDispatch } from 'react-redux';
-import { Setloader } from '../../redux/reducer/loadersSlice';
 
 
 const NavCategories = () => {
 
   const [categories, setCategories] = useState([]);
-  const dispatch = useDispatch();
 
   useEffect(() => {
-<<<<<<< HEAD
-    // Check if cached data is available in localStorage
-    const cachedCategories = JSON.parse(localStorage.getItem('cachedCategories'));
-
-    if (cachedCategories) {
-      setCategories(cachedCategories);
-    } else {
-      const fetchCategories = async () => {
-        try {
-          const response = await GetAllCategories();
-          const fetchedCategories = response.data;
-
-          // Cache the fetched data in localStorage
-          localStorage.setItem('cachedCategories', JSON.stringify(fetchedCategories));
-
-          setCategories(fetchedCategories);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-
-      fetchCategories();
-    }
-  }, []);
-=======
     const fetchCategories = async () => {
       try {
-        dispatch(Setloader(true))
         const response = await GetAllCategories();
         setCategories(response.data);
-        dispatch(Setloader(false))
       } catch (error) {
-        dispatch(Setloader(false))
         console.error("Error fetching data:", error);
       }
     };
 
     fetchCategories();
-  }, [dispatch]);
->>>>>>> ac21ee95d8f0ad5234ef73325fdbff6d7d901e80
-
-  const handleDataUpdateOptimistic = async (updatedData) => {
-    // Update the client-side cache optimistically
-    const updatedCategories = categories.map((category) =>
-      category.id === updatedData.id ? updatedData : category
-    );
-
-    setCategories(updatedCategories);
-
-    try {
-      // Perform the actual data update on the server
-      await updateDataOnServer(updatedData);
-      // Server update successful
-    } catch (error) {
-      console.error('Error updating data on the server:', error);
-      // Handle errors, potentially revert the optimistic update
-      // You may want to handle error scenarios, e.g., show a notification, revert the update, etc.
-      // Depending on your application needs
-    }
-  };
+  }, []);
 
 
-  // DROP DOWN CATEGORIES LOGIC
+
   const [activeCollapsible, setActiveCollapsible] = useState([]);
 
   const handleToggleCollapsible = (index) => {
@@ -101,9 +48,7 @@ const NavCategories = () => {
             <li className='main-category' key={index}>
               <div className='category-icon'>
                 <img src={category.icon} alt='' />
-                <Link to={`/subcategory/${category.id}/${category.label}`}>
-                  {category.label}
-                </Link>
+                {category.label}
               </div>
 
               {category.subcategories && category.subcategories.length > 0 && (
@@ -115,11 +60,7 @@ const NavCategories = () => {
                   {activeCollapsible[index] && (
                     <ul className='sub-category'>
                       {category.subcategories.map((subCategory, subIndex) => (
-                        <li key={subIndex}>
-                          <Link to={`/subcategory/${subCategory.id}/${subCategory.label}`}>
-                            {subCategory.label}
-                          </Link>
-                        </li>
+                        <li key={subIndex}>{subCategory.label}</li>
                       ))}
                     </ul>
                   )}
