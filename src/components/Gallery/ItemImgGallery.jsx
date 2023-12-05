bb6e9c25139b019046a316d3020783a3827ef8f8
Richard Alcaide <richardalcaide16@gmail.com>
2023-12-05 17:54:00 +0800

    all fe update

--- src/components/Gallery/ItemImgGallery.jsx
+++ src/components/Gallery/ItemImgGallery.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './style.scss';
 
 const ItemImgGallery = ({ gallery }) => {
   const [productImgSrc, setProductImgSrc] = useState(gallery[0]);
   const [thumbIndex, setThumbIndex] = useState(0);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const thumbHeight = 76; // Update with the actual height of prod-img-thumb
+
+  const thumbCarouselRef = useRef();
 
   const handleSmallImgClick = (index) => {
     setProductImgSrc(gallery[index]);
@@ -21,6 +24,11 @@ const ItemImgGallery = ({ gallery }) => {
     setThumbIndex(newThumbIndex);
   };
 
+  useEffect(() => {
+    const translateYValue = -thumbIndex * thumbHeight;
+    thumbCarouselRef.current.style.transform = `translateY(${translateYValue}px)`;
+  }, [thumbIndex, thumbHeight]);
+
   const nextSlide = () => {
     const newIndex = (currentIndex + 1) % gallery.length;
     setCurrentIndex(newIndex);
@@ -33,13 +41,15 @@ const ItemImgGallery = ({ gallery }) => {
 
   const isAtFirstImage = currentIndex === 0;
   const isAtLastImage = currentIndex === gallery.length - 1;
+  const isAtFirstThumb = thumbIndex === 0;
+  const isAtLastThumb = thumbIndex === gallery.length - 1;
 
   return (
     <>
       <div className='prod-img-gallery'>
         <div className='group-prod-img-thumb'>
-          <button className="thumb-carousel-arrow top-arrow" onClick={prevThumbSlide} disabled={isAtFirstImage}></button>
-          <div className='thumb-carousel-container' style={{ transform: `translateY(${-thumbIndex * 60}%)` }}>
+          <button className="thumb-carousel-arrow top-arrow" onClick={prevThumbSlide} disabled={isAtFirstThumb}></button>
+          <div className='thumb-carousel-container' ref={thumbCarouselRef}>
             {gallery.map((src, index) => (
               <img
                 key={index}
@@ -50,11 +60,11 @@ const ItemImgGallery = ({ gallery }) => {
               />
             ))}
           </div>
-          <button className="thumb-carousel-arrow bottom-arrow" onClick={nextThumbSlide}></button>
+          <button className="thumb-carousel-arrow bottom-arrow" onClick={nextThumbSlide} disabled={isAtLastThumb}></button>
         </div>
 
         <div className='selected-prod-img-container'>
-          <button className="selected-img-arrow left-arrow" onClick={prevSlide}></button>
+          <button className="selected-img-arrow left-arrow" onClick={prevSlide} disabled={isAtFirstImage}></button>
           <div className="carousel-container" style={{ transform: `translateX(${-currentIndex * 100}%)` }}>
             {gallery.map((src, index) => (
               <img
@@ -66,7 +76,7 @@ const ItemImgGallery = ({ gallery }) => {
               />
             ))}
           </div>
-          <button className="selected-img-arrow right-arrow" onClick={nextSlide}></button>
+          <button className="selected-img-arrow right-arrow" onClick={nextSlide} disabled={isAtLastImage}></button>
         </div>
       </div>
     </>
