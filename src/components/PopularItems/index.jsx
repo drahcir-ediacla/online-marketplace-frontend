41cf6d6dfeb241769840c02bffa585c9f1ea75e7
Richard Alcaide <richardalcaide16@gmail.com>
2023-12-21 14:00:02 +0800

    Popular items update

--- src/components/PopularItems/index.jsx
+++ src/components/PopularItems/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../../apicalls/axios'
+import useAuthentication from '../../hooks/authHook';
 import './style.scss'
 import BtnCategory from  '../../components/Button/BtnCategory'
 import BtnSeeMore from '../../components/Button/BtnSeeMore'
@@ -7,6 +9,69 @@ import popularItemsData from '../../data/popularItemsData'
 import ProductCardSkeleton from '../SkeletonLoader/ProductCardSkeleton'
 
 const PopularItems = () => {
+
+  const [products, setProducts] = useState([]);
+  const [err, setErr] = useState(false);
+  const { user } = useAuthentication();
+
+
+  const addToWishlist = (productId) => {
+    axios.post(`/api/addwishlist/product-${productId}`, {})
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error adding item to wishlist:', error);
+      });
+  };
+
+  const removeFromWishlist = (productId) => {
+    axios.post(`/api/removewishlist/product-${productId}`, {})
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error removing item from wishlist:', error);
+      });
+  };
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+      try {
+        // Fetch the category's data
+        const response = await axios.get('/api/product/most-viewed');
+
+        // Limit the number of items to the first 5
+        const limitedData = response.data.slice(0, 20);
+
+        setProducts(limitedData);
+
+      } catch (error) {
+        console.error('Error fetching category data:', error);
+
+        // Check if the error is due to unauthorized access
+        if (error.response && error.response.status === 500) {
+          return error.message
+          // Handle unauthorized access, e.g., redirect to login
+        } else {
+          // Handle other errors
+          setErr(true); // Depending on your requirements
+        }
+      }
+    }
+    fetchData();
+  }, [])
+
+
+  // Check if data is null or undefined
+  if (!products) {
+    return null; // or return some default content or loading indicator
+  }
+
+
+
   return (
     <>
       <div className="product-section-container">
@@ -25,7 +90,8 @@ const PopularItems = () => {
           <BtnSeeMore label="See More Shoes >>" />
         </div>
         <div>
-          <ProductCarousel data={popularItemsData} />
+        <ProductCarousel data={products} addToWishlist={addToWishlist}
+            removeFromWishlist={removeFromWishlist} userId={user?.id || ''} />
         </div>
       </div>
     </>
