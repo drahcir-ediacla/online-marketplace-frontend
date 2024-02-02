ce0c17faba63ecb6d430cffcb860d2393721303f
Richard Alcaide <richardalcaide16@gmail.com>
2024-02-02 09:26:43 +0800

    sign to send message

--- src/components/RecommendedItems/index.jsx
+++ src/components/RecommendedItems/index.jsx
@@ -116,7 +116,7 @@ const RecommendedItems = ({ userId }) => {
         <div className='product-section-title'>
           <h3>Recommended For You</h3>
         </div>
-        <div className='product-section-btns'>
+        {/* <div className='product-section-btns'>
           <div className='sub-categories-btn'>
             <BtnCategory label="Shoes" className='active' />
             <BtnCategory label="Mobile" />
@@ -126,7 +126,7 @@ const RecommendedItems = ({ userId }) => {
             <BtnCategory label="Chanel" />
           </div>
           <BtnSeeMore label="See More Shoes >>" />
-        </div>
+        </div> */}
         <div className='product-cards-container'>
           {loading &&
             <div className='skeleton-card-container'>
