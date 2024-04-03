d68cc5cde166f0d52b4f7b4b25b0118774627c04
Richard Alcaide <richardalcaide16@gmail.com>
2024-04-03 14:04:20 +0800

    choose us

--- src/components/WhyChooseUs/ChooseUsCard.jsx
+++ src/components/WhyChooseUs/ChooseUsCard.jsx
@@ -5,11 +5,11 @@ import {Link} from 'react-router-dom'
 const ChooseUsCard = (props) => {
   return (
     <>
-    <Link className="why-choose-us-card" to={props.path}>
+    <div className="why-choose-us-card" to={props.path}>
           <figure><img src={props.img} alt="" /></figure>
           <h4>{props.title}</h4>
           <span>{props.text}</span>
-      </Link>
+      </div>
     </>
   )
 }
